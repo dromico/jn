e0fc586c71b91013903604d9e3c36198d7c54197
Onlook <git@onlook.com>
2025-06-10 13:30:47 +0800

    now bring this element in front of the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -561,7 +561,7 @@ export default function Home() {
               </div>
 
               <div
-                className="absolute -bottom-6 -left-6 bg-white p-4 rounded-lg shadow-lg"
+                className="absolute -bottom-6 -left-6 bg-white p-4 rounded-lg shadow-lg z-10"
                 data-oid="1mgjdly"
               >
                 <div className="flex items-center gap-3" data-oid="u_x5dc8">
@@ -1303,6 +1303,7 @@ export default function Home() {
                       d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z"
                       data-oid="_2bv7kq"
                     />
+
                     <path d="M6 9H2V21H6V9Z" data-oid="4h._99:" />
                     <path
                       d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z"
